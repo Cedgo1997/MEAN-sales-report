18edba9d91fd0a949b6d877c862d8e5d8a0b2dd3
César Gonzalez <cedgo1997@gmail.com>
2021-01-05 12:43:57 -0400

    components and routing created

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddSalesComponent } from './components/add-sales/add-sales.component';
+import { EditSalesComponent } from './components/edit-sales/edit-sales.component';
+import { SalesDetailsComponent } from './components/sales-details/sales-details.component';
+import { SalesComponent } from './components/sales/sales.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sales', component: SalesComponent },
+  { path: 'sales-details/:id', component: SalesDetailsComponent },
+  { path: 'add-sales', component: AddSalesComponent },
+  { path: 'edit-sales/:id', component: EditSalesComponent },
+  { path: '', redirectTo: '/sales', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SalesComponent } from './components/sales/sales.component';
+import { SalesDetailsComponent } from './components/sales-details/sales-details.component';
+import { AddSalesComponent } from './components/add-sales/add-sales.component';
+import { EditSalesComponent } from './components/edit-sales/edit-sales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SalesComponent,
+    SalesDetailsComponent,
+    AddSalesComponent,
+    EditSalesComponent
   ],
   imports: [
     BrowserModule,
