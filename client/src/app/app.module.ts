22336c01c6791d5716ed57dc10ea26cece65e186
César Gonzalez <cedgo1997@gmail.com>
2021-01-05 14:09:30 -0400

    charts and socket io client bond

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -25,6 +25,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
+// Ng2-charts
+import { ChartsModule } from 'ng2-charts';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -49,6 +52,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
+    ChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
