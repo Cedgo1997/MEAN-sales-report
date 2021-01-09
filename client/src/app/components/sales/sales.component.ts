31cefe31fc692ceacbf03af4b2a28910992fa893
César Gonzalez <cedgo1997@gmail.com>
2021-01-09 15:07:55 -0400

    Pie chart color fixed temporarily

--- client/src/app/components/sales/sales.component.ts
+++ client/src/app/components/sales/sales.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import * as io from 'socket.io-client';
 import { SalesApiService } from './../../services/sales-api.service';
 
@@ -7,13 +7,14 @@ import { Label } from 'ng2-charts';
 import * as pluginDataLabels from 'chartjs-plugin-datalabels';
 import { Sales } from 'src/app/models/sales.model';
 import { Chart } from 'src/app/models/chart.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sales',
   templateUrl: './sales.component.html',
   styleUrls: ['./sales.component.scss'],
 })
-export class SalesComponent implements OnInit {
+export class SalesComponent implements OnInit, OnDestroy {
   socket = io('http://localhost:4000');
   chartData: Chart[] = [];
 
@@ -38,53 +39,51 @@ export class SalesComponent implements OnInit {
   public pieChartType: ChartType = 'pie';
   public pieChartLegend = true;
   public pieChartPlugins = [pluginDataLabels];
-  public pieChartColors = [];
+  public pieChartColors = [{ backgroundColor: [] }];
 
   displayedColumns: string[] = ['itemId', 'itemName', 'totalPrice'];
   data: Sales[] = [];
   isLoadingResults = true;
+  salesSubs: Subscription;
 
-  constructor(private api: SalesApiService) {}
-
-  ngOnInit(): void {
-    // Initializing Data
+  constructor(private api: SalesApiService) {
     this.getSales();
-    /* this.getChartData(); */
 
     this.socket.on(
       'update-data',
       function (data: any) {
         this.getSales();
-        /* this.getChartData(); */
       }.bind(this)
     );
+  }
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.salesSubs.unsubscribe();
   }
 
   // Getting data from API
 
   getSales() {
-    this.api.getSales().subscribe(
+    this.salesSubs = this.api.getSales().subscribe(
       (res: any) => {
         this.data = res;
         console.log(this.data);
-        this.isLoadingResults = false;
         this.pieChartLabels = [];
         this.pieChartData = [];
         this.pieChartColors = [];
-        const backgrounds = [];
         this.data.forEach((ch, idx) => {
           this.pieChartLabels.push(ch.itemName);
           this.pieChartData.push(ch.totalPrice);
           console.log(idx);
-          backgrounds.push(
-            `rgba(${0 + idx * 10}, ${255 - idx * 20}, ${0 + idx * 10}, 0.3)`
-          );
+          setTimeout(() => {
+            this.pieChartColors[0].backgroundColor.push(
+              `rgba(${0 + idx * 10}, ${255 - idx * 20}, ${0 + idx * 10}, 0.3)`
+            );
+          });
         });
-        this.pieChartColors = [
-          {
-            backgroundColor: backgrounds,
-          },
-        ];
+        this.isLoadingResults = false;
       },
       (err) => {
         console.log(err);
