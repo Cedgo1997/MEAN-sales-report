336a1d4903c3b7d993b8acfa1b1e3d53ba4037ea
César Gonzalez <cedgo1997@gmail.com>
2021-01-15 23:55:11 -0400

    sales reducer entities

--- client/src/app/store/reducers/sales.reducer.ts
+++ client/src/app/store/reducers/sales.reducer.ts
@@ -1,8 +1,12 @@
 import { createReducer, on } from '@ngrx/store';
 import { Sales } from 'src/app/models/sales.model';
 import * as salesActions from './../actions/sales.actions';
+// ENTITIES
+import { EntityAdapter, createEntityAdapter, EntityState } from '@ngrx/entity';
 
-export interface SalesState {
+export const salesAdapter: EntityAdapter<Sales> = createEntityAdapter<Sales>();
+
+export interface SalesState extends EntityState<Sales> {
   loading: boolean;
   loaded: boolean;
   error: any;
@@ -10,6 +14,8 @@ export interface SalesState {
 }
 
 export const initialState: SalesState = {
+  ids: [],
+  entities: {},
   loading: false,
   loaded: false,
   error: null,
@@ -20,14 +26,16 @@ const _salesReducer = createReducer(
   initialState,
 
   on(salesActions.loadSales, (state) => ({ ...state, loading: true })),
-  on(salesActions.loadSalesSuccess, (state, { sales }) => ({
-    ...state,
-    loading: false,
-    loaded: true,
-    sales,
-  })),
+  on(salesActions.loadSalesSuccess, (state, { sales }) =>
+    salesAdapter.setAll(sales, {
+      ...state,
+      loading: false,
+      loaded: true,
+    })
+  ),
   on(salesActions.loadSalesFail, (state, { payload }) => ({
     ...state,
+    entities: {},
     loading: false,
     loaded: false,
     error: payload,
